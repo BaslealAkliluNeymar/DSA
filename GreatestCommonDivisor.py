8a4f2e6f0b0893b16e6b93643dbdaf4f71194964
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2023-12-17 13:18:25 +0300

    GCD Euclidean

--- GreatestCommonDivisor.py
+++ GreatestCommonDivisor.py
@@ -1,2 +1,12 @@
-def GreatestCommonDivisor(n, m):
-    
+def GCD(n, m):
+    #Inorder to solve this problem recursively , we need to use the 
+    # Euclidean algorithm
+
+    if n % m == 0:
+        return m
+    else:
+        return GCD(m,n % m)
+
+
+
+print(GCD(144,12))
