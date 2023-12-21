313b9e1446e6bb93911282583f19a3a995e895e9
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2023-12-21 21:03:15 +0300

    Fixed CLL

--- LinkedLists/CircularSinglyLinkedList.py
+++ LinkedLists/CircularSinglyLinkedList.py
@@ -2,8 +2,16 @@ class Clinkedlist:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            if node.next == self.head:
+                break
+            node = node.next
     
-    def CreateClist(self,value):
+    def CreateClist(self, value):
         new_node = Node(value)
         new_node.next  = new_node
         self.head = new_node
@@ -15,3 +23,7 @@ class Node:
         self.value = value
         self.next = None
 
+linkedlist = Clinkedlist()
+linkedlist.CreateClist(1)
+
+print([node.value for node in linkedlist])
