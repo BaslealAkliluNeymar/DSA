50776e0698a0b47262e978add4f687c9fa7a0463
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2023-12-23 13:43:52 +0300

    Stack More Or Less

--- Stack/stackLL.py
+++ Stack/stackLL.py
@@ -1,16 +1,17 @@
 class LL:
     def __init__(self):
         self.head = None
-        self.tail = None
+
     def __iter__(self):
         node = self.head
         while node:
             yield node
             node = node.next
 
+
 class Node:
     def __init__(self,value=None):
-        self.value = None
+        self.value = value
         self.next = None
     
 
@@ -19,26 +20,29 @@ class stack:
         self.linkedlist = LL()
 
     def __str__(self):
-        values = [str(x) for x in self.linkedlist]
+        values = [str(x.value) for x in self.linkedlist]
+        return '\n'.join(values)
         
-
     def push(self,value):
         new_node = Node(value)
         if self.linkedlist.head is None:
             self.linkedlist.head = new_node
             new_node.next = None
         else:
-            new_node.next = self.linkedlist.head.next
+            new_node.next = self.linkedlist.head
             self.linkedlist.head = new_node
     
     def pop(self):
+        if self.isEmpty():
+            return "There is no element in the stack!"
         print(self.linkedlist.head)
         self.linkedlist.head = self.linkedlist.head.next
     
     def peek(self):
         if self.linkedlist.head:
-            return self.linkedlist.head
-        return "Stack is empty"
+            return self.linkedlist.head.value
+        else:
+            return "Stack is empty"
     
     def delete(self):
         self.linkedlist.head = None
@@ -46,12 +50,20 @@ class stack:
     def isEmpty(self):
         if self.linkedlist.head == None:
             return True
-        return False
+        else:
+            return False
     
 
 customstack = stack()
-print(customstack.peek())
-print(stack)
+customstack.push(1)
+customstack.push(2)
+customstack.push(3)
+customstack.push(4)
+
+
+print(customstack)
+
+
 
 
             
