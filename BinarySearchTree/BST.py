9a8353279ee0528a844c6839ed6cee28acc90f11
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2023-12-26 12:55:49 +0300

    Search BST

--- BinarySearchTree/BST.py
+++ BinarySearchTree/BST.py
@@ -30,7 +30,7 @@ def traverse(rootnode):
         traverse(rootnode.right)
 
 def inordertraveral(rootnode):
-    if not rootnode:
+    if rootnode is None:
         return
     else:
         inordertraveral(rootnode.left)
@@ -44,7 +44,21 @@ def postordertraversal(rootnode):
         print(rootnode.value)
         postordertraversal(rootnode.left)
      
-           
+def search(rootnode,value):
+    if rootnode.value == value:
+        print("The Value is Found!")
+    elif rootnode.value > value:
+        if rootnode.value == value:
+            print("The Value has been found")
+        else:
+            search(rootnode.left,value)
+    else:
+        if rootnode.value == value:
+            print("The value has been found")
+        else:
+            search(rootnode.right,value)
+    
+
     
 bst = BST(70)
 insert(bst,90)
@@ -56,5 +70,4 @@ insert(bst,40)
 insert(bst,80)
 insert(bst,100)
 
-
-postordertraversal(bst)
+print(search(bst,30))
