917c0215f4ca93f4a37e8d29dbcd256d8f003222
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2024-01-06 20:23:47 +0300

    AVL is Awesome!

--- BinarySearchTree/MAVL.py
+++ BinarySearchTree/MAVL.py
@@ -3,6 +3,7 @@ class AVL:
         self.value = value
         self.left = None
         self.rigth = None
+        self.height = 1
 
     def __str__(self):
         if self.left is not None:
@@ -40,6 +41,53 @@ def get_balance(node):
         return 0
     return (get_height(node.rigth) - 1) - (get_height(node.left) - 1)
 
+def imp_height(node):
+    if not node:
+        return 
+    return node.height == (get_height(node) - 1)
+
+def searchnode(node,value):
+    if not node:
+        return "Ain't Nothin!"
+    else:
+        if node.value > value:
+            if node.value == value:
+                print('Found left it!')
+            else:
+                searchnode(node.left,value)
+        else:
+            if node.value == value:
+                print('Found rigth it!')
+            else:
+                searchnode(node.rigth,value)
+        
+
+
+def Inordertraversal(node):
+    if not node:
+        return 
+    else:
+        Inordertraversal(node.left)
+        print(node.value)
+        Inordertraversal(node.rigth)
+
+def preordertraversal(node):
+    if not node:
+        return 
+    print(node.value)
+    preordertraversal(node.left)
+    preordertraversal(node.rigth)
+
+def postordertraversal(node):
+    if not node:
+        return 
+    postordertraversal(node.left)
+    postordertraversal(node.rigth)
+    print(node.value)
+
+
+   
+    
 node = AVL(10)
 insert(node,5)
 insert(node,11)
@@ -47,6 +95,8 @@ insert(node,12)
 insert(node,10.5)
 insert(node,13)
 insert(node, 56)
+postordertraversal(node)
 
-print(get_height(node) - 1)
-print(get_balance(node.rigth.left))
+
+searchnode(node,156)
+
